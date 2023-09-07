6a91c878cca980d7d3004f289888f2c1de03603b
jhonnlennon <jhonn1990@hotmail.com>
2023-09-06 23:34:07 -0300

    Concluído aulas com vetores e objetos

--- Sintaxe Básica em JavaScript/Aula 3 - Array e Objetos/script.js
+++ Sintaxe Básica em JavaScript/Aula 3 - Array e Objetos/script.js
@@ -46,3 +46,65 @@ console.log(array2);
 array2.shift();
 console.log(array2);
 
+//unshift, adiciona um item no início do array
+array2.unshift('o que adicionar no inicio?');
+console.log(array2);
+
+//indexOf,retorna índice de um valor
+console.log(array2.indexOf(true));
+
+//splice, remove ou substitui um item pelo índice
+//o primeiro número representa de qual INDICE ele vai começar.
+//o segundo número representa a quantidade ou contagem do qual vai retirar ou substituir.
+array2.splice(0, 2)
+console.log(array2);
+
+//slice, retorna uma parte de um array existente
+//o primeiro número representa de qual INDICE ele vai começar.
+//o segundo número representa a quantidade ou contagem do qual vai adicionar.
+let novoArray = array2.slice(0, 2)
+console.log(novoArray);
+
+
+//=============================================================
+//SOBRE OBJETOS
+//=============================================================
+
+let object = {
+    string: 'string',
+    number: 1,
+    Boolean: true,
+    arrayQualquer: ["UmArrayAqui"],
+    objectInterno:{
+        objectInterno: 'Um Objeto Interno dentro do objeto.'
+    }
+};
+console.log(object);
+
+
+//caso queira acessar uma determinada propriedade devemos:
+console.log(object.Boolean);
+
+//Destruturação de um objeto:
+var stringObjeto = object.string;
+console.log(stringObjeto);
+
+//Serve para qualquer propriedade
+var numberObjeto = object.number;
+console.log(numberObjeto);
+
+//Caso queira buscar por indice um array que esteja dentro de um objeto 
+//Esta indefinido porque de fato não temos dados dentro do do indice 1 conforme o arrayQualquer
+var arrayObjeto = object.arrayQualquer[1];
+console.log(arrayObjeto);
+
+//Pegando uma propriedade interno de outro objeto
+var teste = object.objectInterno.objectInterno;
+console.log(teste);
+
+//Outra forma de desestruturação de objetos é:
+//Declara-se uma variavel e abre um colchetes
+//Em seguida, atribui de qual objeto
+//dentro do colchetes, adicione as propriedade a qual quer buscar.
+var {Boolean, string} = object;
+console.log(Boolean, string);
